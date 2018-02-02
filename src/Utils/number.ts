3f4694719e11ab815eba62d97e81d55d77168e20
Simonas Karuzas <simonas@not.cat>
2018-02-02 23:27:16 +0200

    Number format fixes

--- src/Utils/number.ts
+++ src/Utils/number.ts
@@ -2,7 +2,7 @@ import { Decimal } from 'decimal.js'
 
 export const numberToString = (val1: string, val2: number): string => {
   const amount = val1 ? val1 : '0'
-  const decimals = val2 ? val1 : '0'
+  const decimals = val2 ? val2 : '0'
   const bigAmount = new Decimal(amount)
   const bigMultiplier = new Decimal(10).pow(decimals)
   return  bigAmount.div(bigMultiplier).toFixed(val2 ? val2 : 0)
@@ -10,7 +10,7 @@ export const numberToString = (val1: string, val2: number): string => {
 
 export const powToString = (val1: number, val2: number): string => {
   const value1 = val1 ? val1 : '0'
-  const value2 = val2 ? val1 : '0'
+  const value2 = val2 ? val2 : '0'
   const big1 = new Decimal(value1)
   const bigMultiplier = new Decimal(10).pow(value2)
   return  big1.mul(bigMultiplier).toString()
