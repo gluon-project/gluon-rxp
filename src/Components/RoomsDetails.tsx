aabd33549ced5a39b147f743083465c59ab9a48a
Simonas Karuzas <simonas@not.cat>
2018-09-03 15:38:54 +0200

    Showing last message in room list

--- src/Components/RoomsDetails.tsx
+++ src/Components/RoomsDetails.tsx
@@ -1,7 +1,7 @@
 import RX = require('reactxp')
 import * as Theme from '../Theme'
 import { ListItem, AccountIcon, MatrixLogin, CallToAction } from '../Components'
-
+import { filter } from 'lodash'
 interface Props extends RX.CommonProps {
   startLogin?: () => void
   navigate?: (routeName: string) => void
@@ -16,6 +16,16 @@ interface Props extends RX.CommonProps {
   isLoggingIn?: boolean
   matrixRegister?: (username: string, password: string, baseUrl: string) => void
   isRegistering?: boolean
+}
+
+const lastMessage = (room: MatrixRoom): string => {
+  if (!room) {
+    return ''
+  }
+  const message = filter(room.timeline,
+    (event: MatrixTimelineEvent) => event.type === 'm.room.message' &&
+    event.content && event.content.body && event.content.body.length > 0).pop()
+  return message ? message.content.body.substr(0, 40) : `${room.members.length} Members`
 }
 
 export default class RoomsDetails extends RX.Component<Props, null> {
@@ -56,7 +66,7 @@ export default class RoomsDetails extends RX.Component<Props, null> {
                 key={key}
                 account={{avatar: room.avatarUrl}}
                 title={`${room.name}`}
-                subTitle={`${room.members.length} Members`}
+                subTitle={lastMessage(room)}
                 type={ListItem.type.Default}
                 selected={!this.props.uiTraits.horizontalIsCompact && this.props.routeName !== 'RoomNewForm' && this.props.selectedRoomId
                   && room.id === this.props.selectedRoomId}
